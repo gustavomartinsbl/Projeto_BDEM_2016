9880217dfd7ec2fcb5b410d09fd0072ab037ac5f
gustavomartinsbl <gu_martins@id.uff.br>
2026-09-02 10:56:41 -0300

    script BDEM - SIM - tarefas 1 a 5

--- script_BDEM.R
+++ script_BDEM.R
@@ -80,6 +80,16 @@ table(dados_sim_2$IDADE, useNA = "ifany")
 # Verifique o dicionário do SIM para identificar qual o código das categorias de cada variável
 # Em variáveis quantitativas como IDADE verificar se existem valores como 9999 para NA
 
+dados_sim_2$SEXO[dados_sim_2$SEXO == "0"] = NA
+dados_sim_2$SEXO[dados_sim_2$SEXO == "9"] = NA
+dados_sim_2$ESC2010[dados_sim_2$ESC2010 == "9"] = NA
+dados_sim_2$TPMORTEOCO[dados_sim_2$TPMORTEOCO == "9"] = NA
+dados_sim_2$IDADE[dados_sim_2$IDADE == "999"] = NA
+
+table(dados_sim_2$SEXO, useNA = "ifany")
+table(dados_sim_2$ESC2010, useNA = "ifany")
+table(dados_sim_2$TPMORTEOCO, useNA = "ifany")
+table(dados_sim_2$IDADE, useNA = "ifany")
 
 # Ao terminar a Tarefa 5 commit com a mensagem "script BDEM - SIM - tarefas 1 a 5" e envie para o repositório Projeto_BDEM_2016
 
